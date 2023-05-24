b4178d716db9315dba1142d0b8aae4f5dc750c74
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-05-24 14:14:49 +0000

    mediana (zadanie na 3.0)

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("zaliczenie1");
+    double[] tab = {1,7,4,3,8,10,12,9,11,15};
+    Arrays.sort(tab);
+    for (double x : tab) {
+      System.out.println(x);
+}
+    System.out.println("Mediana wynosi: " + (tab[4]+tab[5])/2);
   }
 }
